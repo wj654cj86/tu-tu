16b1a2dbf5403887dd3c828e1e913ab1d4fe89e8
WilliamTu <wj654cj86@gmail.com>
2022-02-08 15:32:19 +0800

    V1.0.1 更新IO指令新增英文版和牌位用英文字母大寫來寫

--- pages/api/io.js
+++ pages/api/io.js
@@ -261,6 +261,17 @@ let Countries = {
 
 let languages = ['en', 'zh-TW'];
 
+let languagedata = {
+	'rank': { 'en': 'Rank', 'zh-TW': '牌位' },
+	'world': { 'en': 'World', 'zh-TW': '世界' },
+	'ranking': { 'en': ' ranking', 'zh-TW': '排行' },
+	'第': { 'en': '', 'zh-TW': '第' },
+	'名': { 'en': '', 'zh-TW': '名' },
+	'40l': { 'en': '40 line', 'zh-TW': '40層' },
+	'分': { 'en': '', 'zh-TW': '分' },
+	'page': { 'en': 'Player page', 'zh-TW': '個人頁面' },
+};
+
 function gettxtfile(url) {
 	return new Promise((resolve, reject) => {
 		fetch(url)
@@ -297,16 +308,16 @@ export default async function handler(req, res) {
 	let league = data.user.league;
 	let outarr = [];
 	if (league.rank != 'z') {
-		outarr.push(`牌位: ${league.rank}`);
+		outarr.push(`${languagedata.rank[language]}: ${league.rank.toUpperCase()}`);
 	}
 	if (league.rating != -1) {
 		outarr.push(`TR: ${league.rating.toFixed(0)}`);
 		outarr.push(`Glicko: ${league.glicko.toFixed(0)}+${league.rd.toFixed(0)}`);
 	}
 	if (league.rank != 'z') {
-		outarr.push(`世界排行: 第${league.standing}名`);
+		outarr.push(`${languagedata.world[language] + languagedata.ranking[language]}: ${languagedata.第[language] + league.standing + languagedata.名[language]}`);
 		if (data.user.country !== null) {
-			outarr.push(`${Countries[data.user.country][language]}排行: 第${league.standing_local}名`);
+			outarr.push(`${Countries[data.user.country][language] + languagedata.ranking[language]}: ${languagedata.第[language] + league.standing_local + languagedata.名[language]}`);
 		}
 	}
 	if (league.apm != null) {
@@ -316,13 +327,13 @@ export default async function handler(req, res) {
 	}
 	if (data2.records != null) {
 		if (data2.records['40l'].record != null) {
-			outarr.push(`40層: ${(data2.records['40l'].record.endcontext.finalTime / 1000).toFixed(3)}s`);
+			outarr.push(`${languagedata['40l'][language]}: ${(data2.records['40l'].record.endcontext.finalTime / 1000).toFixed(3)}s`);
 		}
 		if (data2.records['blitz'].record != null) {
-			outarr.push(`Blitz: ${data2.records['blitz'].record.endcontext.score.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}分`);
+			outarr.push(`Blitz: ${data2.records['blitz'].record.endcontext.score.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + languagedata.分[language]}`);
 		}
 	}
-	outarr.push(`個人頁面: https://ch.tetr.io/u/${namel}`);
+	outarr.push(`${languagedata.page[language]}: https://ch.tetr.io/u/${namel}`);
 
 	res.status(200).json(`${nameu} => ` + outarr.join(', '))
 }
