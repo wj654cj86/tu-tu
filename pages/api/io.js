96c2c58aee0e1c2801fd525a108ce3dcc1d50342
WilliamTu <wj654cj86@gmail.com>
2022-02-08 14:55:12 +0800

    V1.0.0

---
+++ pages/api/io.js
@@ -0,0 +1,328 @@
+let Countries = {
+	'AF': { 'en': 'Afghanistan', 'zh-TW': '阿富汗' },
+	'AX': { 'en': 'Åland Islands', 'zh-TW': '奧蘭群島' },
+	'AL': { 'en': 'Albania', 'zh-TW': '阿爾巴尼亞' },
+	'DZ': { 'en': 'Algeria', 'zh-TW': '阿爾及利亞' },
+	'AS': { 'en': 'American Samoa', 'zh-TW': '美屬薩摩亞' },
+	'AD': { 'en': 'Andorra', 'zh-TW': '安道爾' },
+	'AO': { 'en': 'Angola', 'zh-TW': '安哥拉' },
+	'AI': { 'en': 'Anguilla', 'zh-TW': '安圭拉' },
+	'AQ': { 'en': 'Antarctica', 'zh-TW': '南極洲' },
+	'AG': { 'en': 'Antigua and Barbuda', 'zh-TW': '安提瓜和巴布達' },
+	'AR': { 'en': 'Argentina', 'zh-TW': '阿根廷' },
+	'AM': { 'en': 'Armenia', 'zh-TW': '亞美尼亞' },
+	'AW': { 'en': 'Aruba', 'zh-TW': '阿魯巴島' },
+	'AU': { 'en': 'Australia', 'zh-TW': '澳大利亞' },
+	'AT': { 'en': 'Austria', 'zh-TW': '奧地利' },
+	'AZ': { 'en': 'Azerbaijan', 'zh-TW': '阿塞拜疆' },
+	'BS': { 'en': 'Bahamas', 'zh-TW': '巴哈馬' },
+	'BH': { 'en': 'Bahrain', 'zh-TW': '巴林' },
+	'BD': { 'en': 'Bangladesh', 'zh-TW': '孟加拉國' },
+	'BB': { 'en': 'Barbados', 'zh-TW': '巴巴多斯' },
+	'BY': { 'en': 'Belarus', 'zh-TW': '白俄羅斯' },
+	'BE': { 'en': 'Belgium', 'zh-TW': '比利時' },
+	'BZ': { 'en': 'Belize', 'zh-TW': '伯利茲' },
+	'BJ': { 'en': 'Benin', 'zh-TW': '貝寧' },
+	'BM': { 'en': 'Bermuda', 'zh-TW': '百慕大' },
+	'BT': { 'en': 'Bhutan', 'zh-TW': '不丹' },
+	'BO': { 'en': 'Bolivia, Plurinational State of', 'zh-TW': '多民族玻利維亞國' },
+	'BA': { 'en': 'Bosnia and Herzegovina', 'zh-TW': '波斯尼亞和黑塞哥維那' },
+	'BW': { 'en': 'Botswana', 'zh-TW': '博茨瓦納' },
+	'BV': { 'en': 'Bouvet Island', 'zh-TW': '布維島' },
+	'BR': { 'en': 'Brazil', 'zh-TW': '巴西' },
+	'IO': { 'en': 'British Indian Ocean Territory', 'zh-TW': '英屬印度洋領地' },
+	'BN': { 'en': 'Brunei Darussalam', 'zh-TW': '文萊達魯薩蘭國' },
+	'BG': { 'en': 'Bulgaria', 'zh-TW': '保加利亞' },
+	'BF': { 'en': 'Burkina Faso', 'zh-TW': '布基納法索' },
+	'BI': { 'en': 'Burundi', 'zh-TW': '布隆迪' },
+	'KH': { 'en': 'Cambodia', 'zh-TW': '柬埔寨' },
+	'CM': { 'en': 'Cameroon', 'zh-TW': '喀麥隆' },
+	'CA': { 'en': 'Canada', 'zh-TW': '加拿大' },
+	'CV': { 'en': 'Cape Verde', 'zh-TW': '佛得角' },
+	'BQ': { 'en': 'Caribbean Netherlands', 'zh-TW': '加勒比海荷蘭' },
+	'KY': { 'en': 'Cayman Islands', 'zh-TW': '開曼群島' },
+	'CF': { 'en': 'Central African Republic', 'zh-TW': '中非共和國' },
+	'TD': { 'en': 'Chad', 'zh-TW': '乍得' },
+	'CL': { 'en': 'Chile', 'zh-TW': '智利' },
+	'CN': { 'en': 'China', 'zh-TW': '中國' },
+	'CX': { 'en': 'Christmas Island', 'zh-TW': '聖誕島' },
+	'CC': { 'en': 'Cocos (Keeling) Islands', 'zh-TW': '科科斯（基林）群島' },
+	'CO': { 'en': 'Colombia', 'zh-TW': '哥倫比亞' },
+	'KM': { 'en': 'Comoros', 'zh-TW': '科摩羅' },
+	'CG': { 'en': 'Congo', 'zh-TW': '剛果' },
+	'CD': { 'en': 'Congo, the Democratic Republic of the', 'zh-TW': '剛果民主共和國' },
+	'CK': { 'en': 'Cook Islands', 'zh-TW': '庫克群島' },
+	'CR': { 'en': 'Costa Rica', 'zh-TW': '哥斯達黎加' },
+	'CI': { 'en': 'Côte d\'Ivoire', 'zh-TW': '科特迪瓦' },
+	'HR': { 'en': 'Croatia', 'zh-TW': '克羅地亞' },
+	'CU': { 'en': 'Cuba', 'zh-TW': '古巴' },
+	'CW': { 'en': 'Curaçao', 'zh-TW': '庫拉索' },
+	'CY': { 'en': 'Cyprus', 'zh-TW': '塞浦路斯' },
+	'CZ': { 'en': 'Czech Republic', 'zh-TW': '捷克共和國' },
+	'DK': { 'en': 'Denmark', 'zh-TW': '丹麥' },
+	'DJ': { 'en': 'Djibouti', 'zh-TW': '吉布提' },
+	'DM': { 'en': 'Dominica', 'zh-TW': '多米尼克' },
+	'DO': { 'en': 'Dominican Republic', 'zh-TW': '多明尼加共和國' },
+	'EC': { 'en': 'Ecuador', 'zh-TW': '厄瓜多爾' },
+	'EG': { 'en': 'Egypt', 'zh-TW': '埃及' },
+	'SV': { 'en': 'El Salvador', 'zh-TW': '薩爾瓦多' },
+	'GB-ENG': { 'en': 'England', 'zh-TW': '英國' },
+	'GQ': { 'en': 'Equatorial Guinea', 'zh-TW': '赤道幾內亞' },
+	'ER': { 'en': 'Eritrea', 'zh-TW': '厄立特里亞' },
+	'EE': { 'en': 'Estonia', 'zh-TW': '愛沙尼亞' },
+	'ET': { 'en': 'Ethiopia', 'zh-TW': '埃塞俄比亞' },
+	'EU': { 'en': 'Europe', 'zh-TW': '歐洲' },
+	'FK': { 'en': 'Falkland Islands (Malvinas)', 'zh-TW': '福克蘭群島（馬爾維納斯）' },
+	'FO': { 'en': 'Faroe Islands', 'zh-TW': '法羅群島' },
+	'FJ': { 'en': 'Fiji', 'zh-TW': '斐濟' },
+	'FI': { 'en': 'Finland', 'zh-TW': '芬蘭' },
+	'FR': { 'en': 'France', 'zh-TW': '法國' },
+	'GF': { 'en': 'French Guiana', 'zh-TW': '法屬圭亞那' },
+	'PF': { 'en': 'French Polynesia', 'zh-TW': '法屬波利尼西亞' },
+	'TF': { 'en': 'French Southern Territories', 'zh-TW': '法屬南部領地' },
+	'GA': { 'en': 'Gabon', 'zh-TW': '加蓬' },
+	'GM': { 'en': 'Gambia', 'zh-TW': '岡比亞' },
+	'GE': { 'en': 'Georgia', 'zh-TW': '喬治亞州' },
+	'DE': { 'en': 'Germany', 'zh-TW': '德國' },
+	'GH': { 'en': 'Ghana', 'zh-TW': '加納' },
+	'GI': { 'en': 'Gibraltar', 'zh-TW': '直布羅陀' },
+	'GR': { 'en': 'Greece', 'zh-TW': '希臘' },
+	'GL': { 'en': 'Greenland', 'zh-TW': '格陵蘭' },
+	'GD': { 'en': 'Grenada', 'zh-TW': '格林納達' },
+	'GP': { 'en': 'Guadeloupe', 'zh-TW': '瓜德羅普' },
+	'GU': { 'en': 'Guam', 'zh-TW': '關島' },
+	'GT': { 'en': 'Guatemala', 'zh-TW': '危地馬拉' },
+	'GG': { 'en': 'Guernsey', 'zh-TW': '根西島' },
+	'GN': { 'en': 'Guinea', 'zh-TW': '幾內亞' },
+	'GW': { 'en': 'Guinea-Bissau', 'zh-TW': '幾內亞比紹' },
+	'GY': { 'en': 'Guyana', 'zh-TW': '圭亞那' },
+	'HT': { 'en': 'Haiti', 'zh-TW': '海地' },
+	'HM': { 'en': 'Heard Island and McDonald Islands', 'zh-TW': '赫德島和麥克唐納群島' },
+	'VA': { 'en': 'Holy See (Vatican City State)', 'zh-TW': '聖座（梵蒂岡城邦）' },
+	'HN': { 'en': 'Honduras', 'zh-TW': '洪都拉斯' },
+	'HK': { 'en': 'Hong Kong', 'zh-TW': '香港' },
+	'HU': { 'en': 'Hungary', 'zh-TW': '匈牙利' },
+	'IS': { 'en': 'Iceland', 'zh-TW': '冰島' },
+	'IN': { 'en': 'India', 'zh-TW': '印度' },
+	'ID': { 'en': 'Indonesia', 'zh-TW': '印度尼西亞' },
+	'IR': { 'en': 'Iran, Islamic Republic of', 'zh-TW': '伊朗伊斯蘭共和國' },
+	'IQ': { 'en': 'Iraq', 'zh-TW': '伊拉克' },
+	'IE': { 'en': 'Ireland', 'zh-TW': '愛爾蘭' },
+	'IM': { 'en': 'Isle of Man', 'zh-TW': '馬恩島' },
+	'IL': { 'en': 'Israel', 'zh-TW': '以色列' },
+	'IT': { 'en': 'Italy', 'zh-TW': '意大利' },
+	'JM': { 'en': 'Jamaica', 'zh-TW': '牙買加' },
+	'JP': { 'en': 'Japan', 'zh-TW': '日本' },
+	'JE': { 'en': 'Jersey', 'zh-TW': '球衣' },
+	'JO': { 'en': 'Jordan', 'zh-TW': '約旦' },
+	'KZ': { 'en': 'Kazakhstan', 'zh-TW': '哈薩克斯坦' },
+	'KE': { 'en': 'Kenya', 'zh-TW': '肯尼亞' },
+	'KI': { 'en': 'Kiribati', 'zh-TW': '基里巴斯' },
+	'KP': { 'en': 'Korea, Democratic People\'s Republic of', 'zh-TW': '朝鮮民主主義人民共和國' },
+	'KR': { 'en': 'Korea, Republic of', 'zh-TW': '大韓民國' },
+	'XK': { 'en': 'Kosovo', 'zh-TW': '科索沃' },
+	'KW': { 'en': 'Kuwait', 'zh-TW': '科威特' },
+	'KG': { 'en': 'Kyrgyzstan', 'zh-TW': '吉爾吉斯斯坦' },
+	'LA': { 'en': 'Lao People\'s Democratic Republic', 'zh-TW': '老撾人民民主共和國' },
+	'LV': { 'en': 'Latvia', 'zh-TW': '拉脫維亞' },
+	'LB': { 'en': 'Lebanon', 'zh-TW': '黎巴嫩' },
+	'LS': { 'en': 'Lesotho', 'zh-TW': '萊索托' },
+	'LR': { 'en': 'Liberia', 'zh-TW': '利比里亞' },
+	'LY': { 'en': 'Libya', 'zh-TW': '利比亞' },
+	'LI': { 'en': 'Liechtenstein', 'zh-TW': '列支敦士登' },
+	'LT': { 'en': 'Lithuania', 'zh-TW': '立陶宛' },
+	'LU': { 'en': 'Luxembourg', 'zh-TW': '盧森堡' },
+	'MO': { 'en': 'Macao', 'zh-TW': '澳門' },
+	'MK': { 'en': 'Macedonia, the former Yugoslav Republic of', 'zh-TW': '馬其頓，前南斯拉夫共和國' },
+	'MG': { 'en': 'Madagascar', 'zh-TW': '馬達加斯加' },
+	'MW': { 'en': 'Malawi', 'zh-TW': '馬拉維' },
+	'MY': { 'en': 'Malaysia', 'zh-TW': '馬來西亞' },
+	'MV': { 'en': 'Maldives', 'zh-TW': '馬爾代夫' },
+	'ML': { 'en': 'Mali', 'zh-TW': '馬里' },
+	'MT': { 'en': 'Malta', 'zh-TW': '馬耳他' },
+	'MH': { 'en': 'Marshall Islands', 'zh-TW': '馬紹爾群島' },
+	'MQ': { 'en': 'Martinique', 'zh-TW': '馬提尼克島' },
+	'MR': { 'en': 'Mauritania', 'zh-TW': '毛里塔尼亞' },
+	'MU': { 'en': 'Mauritius', 'zh-TW': '毛里求斯' },
+	'YT': { 'en': 'Mayotte', 'zh-TW': '馬約特島' },
+	'MX': { 'en': 'Mexico', 'zh-TW': '墨西哥' },
+	'FM': { 'en': 'Micronesia, Federated States of', 'zh-TW': '密克羅尼西亞聯邦' },
+	'MD': { 'en': 'Moldova, Republic of', 'zh-TW': '摩爾多瓦共和國' },
+	'MC': { 'en': 'Monaco', 'zh-TW': '摩納哥' },
+	'ME': { 'en': 'Montenegro', 'zh-TW': '黑山' },
+	'MA': { 'en': 'Morocco', 'zh-TW': '摩洛哥' },
+	'MN': { 'en': 'Mongolia', 'zh-TW': '蒙古' },
+	'MS': { 'en': 'Montserrat', 'zh-TW': '蒙特塞拉特' },
+	'MZ': { 'en': 'Mozambique', 'zh-TW': '莫桑比克' },
+	'MM': { 'en': 'Myanmar', 'zh-TW': '緬甸' },
+	'NA': { 'en': 'Namibia', 'zh-TW': '納米比亞' },
+	'NR': { 'en': 'Nauru', 'zh-TW': '瑙魯' },
+	'NP': { 'en': 'Nepal', 'zh-TW': '尼泊爾' },
+	'NL': { 'en': 'Netherlands', 'zh-TW': '荷蘭' },
+	'AN': { 'en': 'Netherlands Antilles', 'zh-TW': '荷屬安的列斯' },
+	'NC': { 'en': 'New Caledonia', 'zh-TW': '新喀裡多尼亞' },
+	'NZ': { 'en': 'New Zealand', 'zh-TW': '新西蘭' },
+	'NI': { 'en': 'Nicaragua', 'zh-TW': '尼加拉瓜' },
+	'NE': { 'en': 'Niger', 'zh-TW': '尼日爾' },
+	'NG': { 'en': 'Nigeria', 'zh-TW': '尼日利亞' },
+	'NU': { 'en': 'Niue', 'zh-TW': '紐埃' },
+	'NF': { 'en': 'Norfolk Island', 'zh-TW': '諾福克島' },
+	'GB-NIR': { 'en': 'Northern Ireland', 'zh-TW': '北愛爾蘭' },
+	'MP': { 'en': 'Northern Mariana Islands', 'zh-TW': '北馬里亞納群島' },
+	'NO': { 'en': 'Norway', 'zh-TW': '挪威' },
+	'OM': { 'en': 'Oman', 'zh-TW': '阿曼' },
+	'PK': { 'en': 'Pakistan', 'zh-TW': '巴基斯坦' },
+	'PW': { 'en': 'Palau', 'zh-TW': '帕勞' },
+	'PS': { 'en': 'Palestine', 'zh-TW': '巴勒斯坦' },
+	'PA': { 'en': 'Panama', 'zh-TW': '巴拿馬' },
+	'PG': { 'en': 'Papua New Guinea', 'zh-TW': '巴布亞新幾內亞' },
+	'PY': { 'en': 'Paraguay', 'zh-TW': '巴拉圭' },
+	'PE': { 'en': 'Peru', 'zh-TW': '秘魯' },
+	'PH': { 'en': 'Philippines', 'zh-TW': '菲律賓' },
+	'PN': { 'en': 'Pitcairn', 'zh-TW': '皮特凱恩' },
+	'PL': { 'en': 'Poland', 'zh-TW': '波蘭' },
+	'PT': { 'en': 'Portugal', 'zh-TW': '葡萄牙' },
+	'PR': { 'en': 'Puerto Rico', 'zh-TW': '波多黎各' },
+	'QA': { 'en': 'Qatar', 'zh-TW': '卡塔爾' },
+	'RE': { 'en': 'Réunion', 'zh-TW': '團圓' },
+	'RO': { 'en': 'Romania', 'zh-TW': '羅馬尼亞' },
+	'RU': { 'en': 'Russian Federation', 'zh-TW': '俄羅斯聯邦' },
+	'RW': { 'en': 'Rwanda', 'zh-TW': '盧旺達' },
+	'BL': { 'en': 'Saint Barthélemy', 'zh-TW': '聖巴泰勒米' },
+	'SH': { 'en': 'Saint Helena, Ascension and Tristan da Cunha', 'zh-TW': '聖赫勒拿、阿森松和特里斯坦達庫尼亞' },
+	'KN': { 'en': 'Saint Kitts and Nevis', 'zh-TW': '聖基茨和尼維斯' },
+	'LC': { 'en': 'Saint Lucia', 'zh-TW': '聖盧西亞' },
+	'MF': { 'en': 'Saint Martin', 'zh-TW': '聖馬丁' },
+	'PM': { 'en': 'Saint Pierre and Miquelon', 'zh-TW': '聖皮埃爾和密克隆' },
+	'VC': { 'en': 'Saint Vincent and the Grenadines', 'zh-TW': '聖文森特和格林納丁斯' },
+	'WS': { 'en': 'Samoa', 'zh-TW': '薩摩亞' },
+	'SM': { 'en': 'San Marino', 'zh-TW': '聖馬力諾' },
+	'ST': { 'en': 'Sao Tome and Principe', 'zh-TW': '聖多美和普林西比' },
+	'SA': { 'en': 'Saudi Arabia', 'zh-TW': '沙特阿拉伯' },
+	'GB-SCT': { 'en': 'Scotland', 'zh-TW': '蘇格蘭' },
+	'SN': { 'en': 'Senegal', 'zh-TW': '塞內加爾' },
+	'RS': { 'en': 'Serbia', 'zh-TW': '塞爾維亞' },
+	'SC': { 'en': 'Seychelles', 'zh-TW': '塞舌爾' },
+	'SL': { 'en': 'Sierra Leone', 'zh-TW': '塞拉利昂' },
+	'SG': { 'en': 'Singapore', 'zh-TW': '新加坡' },
+	'SX': { 'en': 'Sint Maarten (Dutch part)', 'zh-TW': '聖馬丁（荷蘭部分）' },
+	'SK': { 'en': 'Slovakia', 'zh-TW': '斯洛伐克' },
+	'SI': { 'en': 'Slovenia', 'zh-TW': '斯洛文尼亞' },
+	'SB': { 'en': 'Solomon Islands', 'zh-TW': '所羅門群島' },
+	'SO': { 'en': 'Somalia', 'zh-TW': '索馬里' },
+	'ZA': { 'en': 'South Africa', 'zh-TW': '南非' },
+	'GS': { 'en': 'South Georgia and the South Sandwich Islands', 'zh-TW': '南喬治亞島和南桑威奇群島' },
+	'SS': { 'en': 'South Sudan', 'zh-TW': '南蘇丹' },
+	'ES': { 'en': 'Spain', 'zh-TW': '西班牙' },
+	'LK': { 'en': 'Sri Lanka', 'zh-TW': '斯里蘭卡' },
+	'SD': { 'en': 'Sudan', 'zh-TW': '蘇丹' },
+	'SR': { 'en': 'Suriname', 'zh-TW': '蘇里南' },
+	'SJ': { 'en': 'Svalbard and Jan Mayen Islands', 'zh-TW': '斯瓦爾巴群島和揚馬延群島' },
+	'SZ': { 'en': 'Swaziland', 'zh-TW': '斯威士蘭' },
+	'SE': { 'en': 'Sweden', 'zh-TW': '瑞典' },
+	'CH': { 'en': 'Switzerland', 'zh-TW': '瑞士' },
+	'SY': { 'en': 'Syrian Arab Republic', 'zh-TW': '阿拉伯敘利亞共和國' },
+	'TW': { 'en': 'Taiwan', 'zh-TW': '台灣' },
+	'TJ': { 'en': 'Tajikistan', 'zh-TW': '塔吉克斯坦' },
+	'TZ': { 'en': 'Tanzania, United Republic of', 'zh-TW': '坦桑尼亞聯合共和國' },
+	'TH': { 'en': 'Thailand', 'zh-TW': '泰國' },
+	'TL': { 'en': 'Timor-Leste', 'zh-TW': '東帝汶' },
+	'TG': { 'en': 'Togo', 'zh-TW': '多哥' },
+	'TK': { 'en': 'Tokelau', 'zh-TW': '托克勞' },
+	'TO': { 'en': 'Tonga', 'zh-TW': '湯加' },
+	'TT': { 'en': 'Trinidad and Tobago', 'zh-TW': '特立尼達和多巴哥' },
+	'TN': { 'en': 'Tunisia', 'zh-TW': '突尼斯' },
+	'TR': { 'en': 'Turkey', 'zh-TW': '火雞' },
+	'TM': { 'en': 'Turkmenistan', 'zh-TW': '土庫曼斯坦' },
+	'TC': { 'en': 'Turks and Caicos Islands', 'zh-TW': '特克斯和凱科斯群島' },
+	'TV': { 'en': 'Tuvalu', 'zh-TW': '圖瓦盧' },
+	'UG': { 'en': 'Uganda', 'zh-TW': '烏干達' },
+	'UA': { 'en': 'Ukraine', 'zh-TW': '烏克蘭' },
+	'AE': { 'en': 'United Arab Emirates', 'zh-TW': '阿拉伯聯合酋長國' },
+	'GB': { 'en': 'United Kingdom', 'zh-TW': '英國' },
+	'US': { 'en': 'United States', 'zh-TW': '美國' },
+	'UY': { 'en': 'Uruguay', 'zh-TW': '烏拉圭' },
+	'UM': { 'en': 'US Minor Outlying Islands', 'zh-TW': '美國本土外小島嶼' },
+	'UZ': { 'en': 'Uzbekistan', 'zh-TW': '烏茲別克斯坦' },
+	'VU': { 'en': 'Vanuatu', 'zh-TW': '瓦努阿圖' },
+	'VE': { 'en': 'Venezuela, Bolivarian Republic of', 'zh-TW': '委內瑞拉玻利瓦爾共和國' },
+	'VN': { 'en': 'Vietnam', 'zh-TW': '越南' },
+	'VG': { 'en': 'Virgin Islands, British', 'zh-TW': '英屬維爾京群島' },
+	'VI': { 'en': 'Virgin Islands, U.S.', 'zh-TW': '美國維爾京群島' },
+	'GB-WLS': { 'en': 'Wales', 'zh-TW': '威爾士' },
+	'WF': { 'en': 'Wallis and Futuna Islands', 'zh-TW': '瓦利斯和富圖納群島' },
+	'EH': { 'en': 'Western Sahara', 'zh-TW': '撒哈拉沙漠西部' },
+	'YE': { 'en': 'Yemen', 'zh-TW': '也門' },
+	'ZM': { 'en': 'Zambia', 'zh-TW': '贊比亞' },
+	'ZW': { 'en': 'Zimbabwe', 'zh-TW': '津巴布韋' },
+	'XX': { 'en': 'Unknown', 'zh-TW': '未知' },
+	'XM': { 'en': 'The Moon', 'zh-TW': '月亮' }
+};
+
+let languages = ['en', 'zh-TW'];
+
+function gettxtfile(url) {
+	return new Promise((resolve, reject) => {
+		fetch(url)
+			.then((response) => {
+				return response.text();
+			})
+			.then((text) => {
+				resolve(text);
+			});
+	});
+}
+
+export default async function handler(req, res) {
+	let param = req.query;
+	let language = param.language;
+	if (languages.indexOf(language) == -1) language = 'zh-TW';
+	let name = param.name;
+	if (name === undefined || name == "") name = param.streamer;
+	let namel;
+	let nameu;
+	let obj;
+	while (1) {
+		namel = name.toLowerCase();
+		nameu = name.toUpperCase();
+		obj = JSON.parse(await gettxtfile("https://ch.tetr.io/api/users/" + namel));
+		if (obj.success == true) break;
+		if (name == param.streamer)
+			name = 'tu-tu';
+		else
+			name = param.streamer;
+	}
+	let data = obj.data;
+	let data2 = JSON.parse(await gettxtfile("https://ch.tetr.io/api/users/" + namel + '/records')).data;
+	let league = data.user.league;
+	let outarr = [];
+	if (league.rank != 'z') {
+		outarr.push(`牌位: ${league.rank}`);
+	}
+	if (league.rating != -1) {
+		outarr.push(`TR: ${league.rating.toFixed(0)}`);
+		outarr.push(`Glicko: ${league.glicko.toFixed(0)}+${league.rd.toFixed(0)}`);
+	}
+	if (league.rank != 'z') {
+		outarr.push(`世界排行: 第${league.standing}名`);
+		if (data.user.country !== null) {
+			outarr.push(`${Countries[data.user.country][language]}排行: 第${league.standing_local}名`);
+		}
+	}
+	if (league.apm != null) {
+		outarr.push(`APM: ${league.apm}`);
+		outarr.push(`PPS: ${league.pps}`);
+		outarr.push(`VS: ${league.vs}`);
+	}
+	if (data2.records != null) {
+		if (data2.records['40l'].record != null) {
+			outarr.push(`40層: ${(data2.records['40l'].record.endcontext.finalTime / 1000).toFixed(3)}s`);
+		}
+		if (data2.records['blitz'].record != null) {
+			outarr.push(`Blitz: ${data2.records['blitz'].record.endcontext.score.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}分`);
+		}
+	}
+	outarr.push(`個人頁面: https://ch.tetr.io/u/${namel}`);
+
+	res.status(200).json(`${nameu} => ` + outarr.join(', '))
+}
