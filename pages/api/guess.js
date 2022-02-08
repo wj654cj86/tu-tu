f4a2ba95b3301407190181fcc41ebf9007af3899
WilliamTu <wj654cj86@gmail.com>
2022-02-08 20:35:31 +0800

    V1.1.2 猜數字修改為不回應名稱

--- pages/api/guess.js
+++ pages/api/guess.js
@@ -36,7 +36,7 @@ function gettxtfile(url) {
 const 獎金 = 10000;
 
 export default async function handler(req, res) {
-	let { urlkey, key, streamer, player, playername, answer, token } = req.query;
+	let { urlkey, key, streamer, player, answer, token } = req.query;
 	try {
 		const supabase = createClient(
 			`https://${urlkey}.supabase.co`,
@@ -44,7 +44,6 @@ export default async function handler(req, res) {
 		);
 		if (streamer === undefined || streamer == "") throw 'error';
 		if (player === undefined || player == "") throw 'error';
-		if (playername === undefined || playername == "") throw 'error';
 		let { data } = await supabase.from("guess").select();
 		let one = data.find(element => element.name == streamer);
 		if (one === undefined) {
@@ -57,11 +56,11 @@ export default async function handler(req, res) {
 		let 輸出 = '';
 		let 答案 = one.answer;
 		if (猜測 == 'null' || 猜測 == null) {
-			輸出 = playername + '，你沒有給數字！';
+			輸出 = '你沒有給數字！';
 		} else if (猜測.length != 4) {
-			輸出 = playername + '，你給的數字有錯！';
+			輸出 = '你給的數字有錯！';
 		} else if (重複數字(猜測)) {
-			輸出 = playername + '，你給到重覆數字，不給你提示！';
+			輸出 = '你給到重覆數字，不給你提示！';
 		} else {
 			let a = 0, b = 0;
 			for (let i = 0; i < 4; i++)
@@ -81,7 +80,7 @@ export default async function handler(req, res) {
 				輸出 += '0A';
 			if (a == 4) {
 				await supabase.from("guess").update({ answer: 出題() }).match({ name: streamer });
-				輸出 = `恭喜${playername}猜中數字！獲得${獎金}喵喵幣！`;
+				輸出 = `恭喜你猜中數字！獲得${獎金}喵喵幣！`;
 				await gettxtfile(`https://api.jebaited.net/addPoints/${token}/${player}/${獎金}`);
 			}
 		}
