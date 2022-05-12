fcd14bb7c6c22f5f3a3f16397c46daf1b1a10e9b
WilliamTu <wj654cj86@gmail.com>
2022-05-12 15:16:28 +0800

    V1.1.9 新增揪團回推廣功能

---
+++ pages/api/raid.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+	let { name } = req.query;
+	let 帳號 = '';
+	let 暱稱 = '';
+	if (name.indexOf('(') != -1) {
+		let 括弧 = name.indexOf(' (');
+		帳號 = name.substr(括弧).replace(' (', '').replace(')', '').toLowerCase();
+		暱稱 = name.substr(0, 括弧);
+	} else {
+		帳號 = name.toLowerCase();
+		暱稱 = name;
+	}
+	res.status(200).json(`{'帳號':${暱稱},'暱稱':${帳號}}`);
+}
